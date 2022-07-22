77b0f550300236d78134c561e54098369bab6639
Yuan <nguyenshort@gmail.com>
2022-07-22 10:18:40 +0700

    cm

--- src/components/VideoCall.vue
+++ src/components/VideoCall.vue
@@ -2,11 +2,19 @@
   <div>
     <h4>{{ uid }}</h4>
     <div ref="el" class="video-call"></div>
+
+    <div>
+      <pre>
+        audio: {{ localAudioTrack.getTrackId() }}
+        video: {{ localVideoTrack.getTrackId() }}
+      </pre>
+    </div>
+
   </div>
 </template>
 
 <script lang="ts" setup>
-import {nextTick, onMounted, ref} from "vue";
+import {nextTick, onMounted, ref, watch} from "vue";
 import {ILocalTrack} from "agora-rtc-sdk-ng";
 
 const props = defineProps<{
@@ -21,14 +29,19 @@ const emit = defineEmits(['logger'])
 
 onMounted(() => nextTick(() => {
   props.localVideoTrack?.play(el.value)
-
-  if (props.localAudioTrack) {
-
-    props.localAudioTrack.play()
-
-  }
-
+  props.localAudioTrack?.play()
 }))
+
+watch(() => props.localVideoTrack, (track) => {
+  if (track) {
+    track.play(el.value)
+  }
+})
+watch(() => props.localAudioTrack, (track) => {
+  if (track) {
+    track.play()
+  }
+})
 
 </script>
 
